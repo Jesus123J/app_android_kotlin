54a51d0a7b26b4d726ddd82cfaf6fc5a1089b817
Thiago <gutierrezdelatorrej30@gmail.com>
2025-07-02 17:48:30 -0500

    Larning Kotlin layouts by adjusting padding

--- service_app/app/src/main/java/com/utp/service_app/MainActivity.kt
+++ service_app/app/src/main/java/com/utp/service_app/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.utp.service_app
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,6 +11,8 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var button : Button;
+    private lateinit var text : TextView;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +23,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        button = findViewById(R.id.button);
+         text  = findViewById(R.id.textView);
+        button.setOnClickListener {
+            val value = sum(1,2)
+            Log.i("Component" ,  value.toString())
+
+            text.text = value.toString()
+        }
     }
+
+
+    fun sum(x: Int , y: Int) : Int{
+        return (x + y);
+    }
+
 }
